d7b8a5984f1c96a26b23cf8a41f71663ba4073db
Erwann Mest <m@kud.io>
2015-12-18 17:21:22 +0100

    Update a bunch of things:
    - New cover
    - Reorganisation of style files
    - Better links

--- tasks/images.js
+++ tasks/images.js
@@ -3,7 +3,7 @@ var imagemin = require('gulp-imagemin')
 
 module.exports = function() {
 
-  return gulp.src('src/images/**/*')
+  return gulp.src(['src/images/**/*', '!src/images/_covers/**/*'])
     .pipe( imagemin() )
     .pipe( gulp.dest('dist/images') )
 
