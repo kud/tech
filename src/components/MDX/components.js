6f0a3afcc6605eca9581de8862f082356b663cc0
Erwann Mest <m@kud.io>
2020-05-17 18:58:10 +0200

    feat(): handle in a better way MDX

---
+++ src/components/MDX/components.js
@@ -0,0 +1,110 @@
+import styled from "@emotion/styled"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import theme from "prism-react-renderer/themes/oceanicNext"
+
+import PostLayout from "~/components/Layout/Post"
+
+const Code = styled.code`
+  background: #33363e;
+  color: rgb(255, 255, 255);
+  padding: 0.2rem 0.5rem;
+  font-size: 1.6rem;
+  line-height: 1.4;
+  vertical-align: 1px;
+  border-radius: 4px;
+  font-family: monospace;
+  border: 4px solid #33363e;
+  border-top-width: 2px;
+  border-bottom-width: 2px;
+`
+
+const Blockquote = styled.blockquote`
+  margin: 0;
+  padding: 0.5rem 2rem;
+  border-radius: 4px;
+  background-color: #f3f3f3;
+  display: flex;
+  align-items: center;
+  font-style: italic;
+
+  &:before {
+    content: "💬";
+    margin-right: 2rem;
+    font-style: normal;
+  }
+`
+
+const Pre = styled.pre`
+  text-align: left;
+  margin: 3rem 0;
+  padding: 1rem 2rem 1rem 0rem;
+  overflow: scroll;
+  border-radius: 4px;
+`
+
+const Line = styled.div`
+  display: flex;
+`
+
+const LineNo = styled.span`
+  text-align: right;
+  padding-right: 1em;
+  user-select: none;
+  opacity: 0.5;
+  width: 6rem;
+`
+
+const LineContent = styled.span`
+  padding-right: 2rem;
+`
+
+const components = {
+  wrapper: PostLayout,
+
+  inlineCode: ({ children }) => <Code>{children}</Code>,
+
+  blockquote: ({ children }) => <Blockquote>{children}</Blockquote>,
+
+  pre: ({ children }) => {
+    const className = children.props.className || ""
+    const matches = className.match(/language-(?<lang>.*)/)
+
+    return (
+      <Highlight
+        {...defaultProps}
+        theme={theme}
+        code={children.props.children}
+        language={
+          matches && matches.groups && matches.groups.lang
+            ? matches.groups.lang
+            : ""
+        }
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <Pre className={className} style={style}>
+            {tokens.map((line, i) => {
+              // @HACK: for a reason I don't know, there's an empty line in the end of any code part
+              if (line[0].content === "" && i === tokens.length - 1) {
+                return undefined
+              }
+
+              return (
+                <Line key={i} {...getLineProps({ line, key: i })}>
+                  <LineNo>{i + 1}</LineNo>
+
+                  <LineContent>
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </LineContent>
+                </Line>
+              )
+            })}
+          </Pre>
+        )}
+      </Highlight>
+    )
+  },
+}
+
+export default components
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,8 @@
 import React from "react"
 import Head from "next/head"
 import { MDXProvider } from "@mdx-js/react"
-import styled from "@emotion/styled"
-import Highlight, { defaultProps } from "prism-react-renderer"
-import theme from "prism-react-renderer/themes/oceanicNext"
+
+import components from "~/components/MDX/components"
 
 // Global styles must be necessarily here: https://github.com/zeit/next.js/blob/master/errors/css-global.md
 import "normalize.css"
@@ -15,107 +14,6 @@ import "~/styles/define-rem.css"
 import "~/styles/selection.css"
 import "~/styles/global-animation.css"
 import "~/styles/base.css"
-
-import PostLayout from "~/components/Layout/Post"
-
-const Code = styled.code`
-  background: #1b2b34;
-  color: rgb(255, 255, 255);
-  padding: 0.2rem 0.5rem;
-  font-size: 1.6rem;
-  line-height: 1.4;
-  vertical-align: 1px;
-  border-radius: 4px;
-  font-family: monospace;
-`
-
-const Blockquote = styled.blockquote`
-  margin: 0;
-  padding: 0.5rem 2rem;
-  border-radius: 4px;
-  background-color: #f3f3f3;
-  display: flex;
-  align-items: center;
-  font-style: italic;
-
-  &:before {
-    content: "💬";
-    margin-right: 2rem;
-    font-style: normal;
-  }
-`
-
-const Pre = styled.pre`
-  text-align: left;
-  margin: 3rem 0;
-  padding: 1rem 2rem;
-  overflow: scroll;
-  border-radius: 4px;
-`
-
-const Line = styled.div`
-  display: flex;
-`
-
-const LineNo = styled.span`
-  text-align: right;
-  padding-right: 1em;
-  user-select: none;
-  opacity: 0.5;
-`
-
-const LineContent = styled.span`
-  padding-right: 2rem;
-`
-
-const components = {
-  wrapper: PostLayout,
-
-  inlineCode: ({ children }) => <Code>{children}</Code>,
-
-  blockquote: ({ children }) => <Blockquote>{children}</Blockquote>,
-
-  pre: ({ children }) => {
-    const className = children.props.className || ""
-    const matches = className.match(/language-(?<lang>.*)/)
-
-    return (
-      <Highlight
-        {...defaultProps}
-        theme={theme}
-        code={children.props.children}
-        language={
-          matches && matches.groups && matches.groups.lang
-            ? matches.groups.lang
-            : ""
-        }
-      >
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <Pre className={className} style={style}>
-            {tokens.map((line, i) => {
-              // @HACK: for a reason I don't know, there's an empty line in the end of any code part
-              if (line[0].content === "" && i === tokens.length - 1) {
-                return undefined
-              }
-
-              return (
-                <Line key={i} {...getLineProps({ line, key: i })}>
-                  <LineNo>{i + 1}</LineNo>
-
-                  <LineContent>
-                    {line.map((token, key) => (
-                      <span key={key} {...getTokenProps({ token, key })} />
-                    ))}
-                  </LineContent>
-                </Line>
-              )
-            })}
-          </Pre>
-        )}
-      </Highlight>
-    )
-  },
-}
 
 const App = ({ Component, pageProps }) => (
   <MDXProvider components={components}>
@@ -133,19 +31,5 @@ const App = ({ Component, pageProps }) => (
     <Component {...pageProps} />
   </MDXProvider>
 )
-
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-
-// App.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext)
-//
-//   console.log(appProps)
-//
-//   return { ...appProps }
-// }
 
 export default App
